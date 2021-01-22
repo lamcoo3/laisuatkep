5034a93a2a89a1053f0e5fe48477823ebf1eaf2f
lamcoo3 <41884142+lamcoo3@users.noreply.github.com>
2021-01-22 14:18:04 +0700

    update

--- my.js
+++ my.js
@@ -1,7 +1,7 @@
 let money = prompt('Nhập số tiền muốn gửi: ');
 let year = prompt('Nhập số năm muốn gửi: ');
 let interest = prompt('Nhập lãi suất: ');
-gocvalai = money * Math.pow(1 + (interest/12), 12 * year)
+goclai = money * Math.pow(1 + (interest/12), 12 * year)
 laikep = gocvalai - money;
 document.write('Lãi kép sau' + year + 'năm là: ', +laikep);
-document.write('Số tiền có được sau' + year + 'năm là: ', +gocvalai);
+document.write('Số tiền có được sau' + year + 'năm là: ', +goclai);
