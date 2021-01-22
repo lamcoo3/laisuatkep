0e8ab605a07fe85de7f433a0165a5e581f79244e
lamcoo3 <41884142+lamcoo3@users.noreply.github.com>
2021-01-22 14:19:36 +0700

    update2

--- my.js
+++ my.js
@@ -2,6 +2,6 @@ let money = prompt('Nhập số tiền muốn gửi: ');
 let year = prompt('Nhập số năm muốn gửi: ');
 let interest = prompt('Nhập lãi suất: ');
 goclai = money * Math.pow(1 + (interest/12), 12 * year)
-laikep = gocvalai - money;
+laikep = goclai - money;
 document.write('Lãi kép sau' + year + 'năm là: ', +laikep);
 document.write('Số tiền có được sau' + year + 'năm là: ', +goclai);
